705d07856ed88d883c3ebf7de5ea441736c99245
Marcin Staniek <marcin.staniek97@gmail.com>
2020-04-19 15:49:04 +0200

    Creating ImpactBlast
    *Using FAttachmentTransformRules

--- 04_BattleTank/BattleTank/Source/Public/Projectile.h
+++ 04_BattleTank/BattleTank/Source/Public/Projectile.h
@@ -26,6 +26,10 @@ public:
 	void LaunchProjectile(float Speed);
 	
 private : 
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+	
 	UProjectileMovementComponent *ProjectileMovement = nullptr;
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
@@ -33,4 +37,7 @@ private :
 	
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UParticleSystemComponent* LaunchBlast = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UParticleSystemComponent* ImpactBlast = nullptr;
 };
