f95ef5a763646479e84537c3bef90a02bd7409af
Marcin Staniek <marcin.staniek97@gmail.com>
2020-05-16 11:35:32 +0200

    UI Widget Interaction

--- VR_LightPainter/Source/VR_LightPainter/UIPointerHandController.cpp
+++ VR_LightPainter/Source/VR_LightPainter/UIPointerHandController.cpp
@@ -3,3 +3,22 @@
 
 #include "UIPointerHandController.h"
 
+#include "InputCoreTypes.h"
+
+AUIPointerHandController::AUIPointerHandController()
+{
+	Pointer = CreateDefaultSubobject<UWidgetInteractionComponent>(TEXT("Pointer"));
+	Pointer->SetupAttachment(GetRootComponent());
+
+
+}
+
+void AUIPointerHandController::TriggerPressed()
+{
+	Pointer->PressPointerKey(EKeys::LeftMouseButton); // Trigger
+}
+
+void AUIPointerHandController::TriggerReleased()
+{
+	Pointer->ReleasePointerKey(EKeys::LeftMouseButton);
+}
--- VR_LightPainter/Source/VR_LightPainter/UIPointerHandController.h
+++ VR_LightPainter/Source/VR_LightPainter/UIPointerHandController.h
@@ -4,7 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "HandControllerBase.h"
+
+#include "Components/WidgetInteractionComponent.h"
+
 #include "UIPointerHandController.generated.h"
+
 
 /**
  * 
@@ -13,5 +17,15 @@ UCLASS()
 class VR_LIGHTPAINTER_API AUIPointerHandController : public AHandControllerBase
 {
 	GENERATED_BODY()
+
+		AUIPointerHandController();
+
+public:
+	virtual void TriggerPressed() override;
+	virtual void TriggerReleased() override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	UWidgetInteractionComponent* Pointer;
 	
 };
