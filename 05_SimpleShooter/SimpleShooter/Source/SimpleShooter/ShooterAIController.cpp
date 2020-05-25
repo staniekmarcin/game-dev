ff917beddf69d07825db12df8bd3d42e88c2d6e2
Marcin Staniek <marcin.staniek97@gmail.com>
2020-05-25 13:20:33 +0200

    AI Aiming

--- 05_SimpleShooter/SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
+++ 05_SimpleShooter/SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
@@ -2,4 +2,15 @@
 
 
 #include "ShooterAIController.h"
+#include "Kismet/GameplayStatics.h"
 
+void AShooterAIController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+    SetFocus(PlayerPawn);
+
+    // TODO turn off focus when AI is dead
+   }
--- 05_SimpleShooter/SimpleShooter/Source/SimpleShooter/ShooterAIController.h
+++ 05_SimpleShooter/SimpleShooter/Source/SimpleShooter/ShooterAIController.h
@@ -13,5 +13,7 @@ UCLASS()
 class SIMPLESHOOTER_API AShooterAIController : public AAIController
 {
 	GENERATED_BODY()
-	
+
+protected:
+    virtual void BeginPlay() override;
 };
